6c938f5b5506afb1d6cbaa261e9a4d0c4f577890
k3nny-chung <k3nny.chung@gmail.com>
2016-04-11 13:42:10 -0400

    First commit

---
+++ PearPortal/due_dilligence/views.py
@@ -0,0 +1,68 @@
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.template import loader
+from django.core.urlresolvers import reverse
+from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Document
+from .forms import DocForm
+
+PAGE_SIZE = 20
+
+@login_required
+def doc_index(request):
+	#docs = Document.objects.filter(processor__in = request.user.materialprocessor_set.all())
+	processors = request.user.materialprocessor_set.all()
+	template = loader.get_template('due_dilligence/docIndex.html')
+	context = { 
+		'processors' : processors, 
+	}
+
+	return HttpResponse(template.render(context, request))
+
+@login_required
+def user_uploaded_docs(request):
+	docs_list = Document.objects.filter(uploaded_by = request.user).order_by('-uploaded_by')
+	paginator = Paginator(docs_list, PAGE_SIZE)
+	page = request.GET.get('page')
+	try:
+		docs = paginator.page(page)
+	except PageNotAnInteger:
+		# if page is not an integer, deliver first page
+		docs = paginator.page(1)
+	except EmptyPage:
+		# if page is out of range, deliver the last page
+		docs = paginator.page(paginator.num_pages)
+			
+	return render(request, 'due_dilligence/userDocs.html', { 'documents': docs })
+
+
+@login_required
+def upload_doc(request):
+	if request.method == "POST":
+		form = DocForm(request.POST, request.FILES)
+		if form.is_valid():
+			doc = form.save(commit=False)
+			doc.uploaded_by = request.user
+			form.save()
+			return HttpResponseRedirect(reverse('duedilligence:index'))
+	else:
+		form = DocForm()
+	return 	render(request, 'due_dilligence/upload.html', { 'form': form })
+
+@login_required
+def upload_doc_xhr(request):
+	if request.method == "POST":
+		form = DocForm(request.POST, request.FILES)
+		if form.is_valid():
+			doc = form.save(commit=False)
+			doc.uploaded_by = request.user
+			form.save()
+			return JsonResponse({ 'success': 'true' })
+		else:
+			return JsonResponse({
+					'success': 'false',
+					'errors': dict(form.errors.items())
+				})
+
+
---
+++ PearPortal/portalenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\KennethChung\Python\Projects\PearPortal\PearPortal\portalenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
