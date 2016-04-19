303ab29fefac28f574b895674a2ec13eb2ed6b8c
k3nny-chung <k3nny.chung@gmail.com>
2016-04-19 13:22:03 -0400

    clean

--- PearPortal/portalenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\KennethChung\Python\Projects\PearPortal\PearPortal\portalenv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
